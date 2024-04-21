2095e64d01acee3c6055e2d6c556866e16cd1718
zahran <zykkhan.bd@gmail.com>
2024-04-21 16:23:16 -0400

    structured api

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -12,5 +12,16 @@ snippet
 
 const API_KEY = 'AIzaSyDZIbkJ6_vQdmNk60bACpUzNY29jQQUtag';
 
+const youtubeAPI = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/', // Base URL for the YouTube Data API
+    params : {
+        key: API_KEY, // API key required for accessing the YouTube Data API
+        part: 'snippet', // specifies the part of the resource that the API response will include; snippet object contains basic details about the video
+        maxResults: 10, // maximum number of items that should be returned in the result set
+    },
+});
+
+export default youtubeAPI;
+
 
 
