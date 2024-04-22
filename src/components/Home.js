e7584026d98bc3c39b4dbd7ac63d8e2b7908781e
zahran <zykkhan.bd@gmail.com>
2024-04-21 21:22:02 -0400

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,3 +3,13 @@
 
 import React from 'react';
 import SearchBar from './SearchBar'; 
+import youtube from '../api/youtube';
+import VideoList from './VideoList';
+
+// Defining a class-based component Home
+class Home extends React.Component {
+
+    
+}
+
+export default Home;
