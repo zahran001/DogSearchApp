98a7ed8ef64fbd7d28bdcfd144599df6ddd2237c
zahran <zykkhan.bd@gmail.com>
2024-04-21 20:28:07 -0400

    used map for the list

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,6 +2,17 @@
 import React from 'react';
 import VideoComponent from './VideoComponent';
 
-const VideoList = () => {
+/* function to render a list of videos by mapping through the videos array and 
+rendering a VideoComponent for each video in the list */
+const VideoList = ({videos, handleVideoSelect}) => {
+    const listVideos = videos.map((video) => {
+        return <VideoComponent video={video} handleVideoSelect={handleVideoSelect} />
+
+    });
+    
+    return (
+        <div>{listVideos}</div>
+    );
 
 };
+export default VideoList;
