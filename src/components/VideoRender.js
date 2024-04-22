3e0987b15fd2a74dbce3fa0c21ea4be9a817e96c
zahran <zykkhan.bd@gmail.com>
2024-04-21 22:15:24 -0400

    test rendering component

--- src/components/VideoRender.js
+++ src/components/VideoRender.js
@@ -1,10 +1,30 @@
 // Render the video when it is selected from the list of videos  
 import React from 'react'
 
-const VideoRender = () => {
-  return (
-    <div>VideoRender</div>
-  )
-}
 
-export default VideoRender
+// functional component to display video details
+const VideoRender = ({ video }) => {
+  if (!video) {
+    return (
+      <div>
+        Search any Dog related video
+      </div>
+    );
+  }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id}`;
+  return (
+    <div>
+      <div>
+        <iframe src={videoSrc} allow="fullscreen" />
+      </div>
+      <div>
+        <h3>{video.snippet.title}</h3>
+        <p>{video.snippet.description}</p>
+        <p>{video.snippet.channelTitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoRender;
